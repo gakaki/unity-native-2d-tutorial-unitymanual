6a405699ae332a18270b8a17224a135f44b6b2c1
gakaki <gakaki@gmail.com>
2013-10-30 22:09:12 +0800

    first commit has 01~04 lessones in the scene

---
+++ Assets/scripts/eventx.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eventx : MonoBehaviour {
+	void AddFire(){
+		UnityEngine.Debug.Log("fire");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
